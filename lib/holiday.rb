2ca382d970775d2ee784669daa3f691a8f36669c
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 20:48:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    puts season.to_s.capitalize! << ":"
+    holiday_hash[season].each do |holiday, supply_array|
+      puts "\t" << holiday.to_s.capitalize! << ": " << holiday.values.
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
