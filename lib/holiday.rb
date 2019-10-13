171d1a959596345fe46d4585899e3d4d566fe815
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 21:11:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,8 +75,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
-
+  holiday_names = []
+  holiday_hash.each do |season, holidays|
+    holiday_names << holidays if holiday_hash[season][holidays].values.include?"BBQ"
+  end
 end
 
 
